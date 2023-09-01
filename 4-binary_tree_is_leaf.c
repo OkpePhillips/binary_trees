2bb809ba45f6ebacd7223afddc65c3e77ddc1c4a
Godwin Okpe <okpegodwin18@yahoo.com>
2023-09-01 07:25:37 -0700

    Minor fix in the file

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,7 +9,7 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
-		return (1);
+		return (0);
 	if (node->parent != NULL)
 	{
 		if (node->left == NULL && node->right == NULL)
@@ -17,5 +17,5 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 		else
 			return (0);
 	}
-	return (0);
+	return (1);
 }
